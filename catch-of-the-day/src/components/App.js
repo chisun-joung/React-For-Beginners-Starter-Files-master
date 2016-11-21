6d41a0603af8665a55062ac0b77ee8a31872f7a3
chisun-joung <csjoung@gmail.com>
2016-11-22 08:44:02 +0900

    ch 20

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,6 +10,7 @@
      constructor() {
          super();
          this.addFish = this.addFish.bind(this);
+         this.updateFish = this.updateFish.bind(this);
          this.loadSamples = this.loadSamples.bind(this);
          this.addToOrder = this.addToOrder.bind(this);
          this.state = {
@@ -50,6 +51,12 @@
          this.setState({fishes_});
      }
 
+     updateFish(key, updatedFish) {
+         const fishes = {...this.state.fishes};
+         fishes[key] = updatedFish;
+         this.setState({ fishes});
+     }
+
      loadSamples() {
          this.setState({
              fishes: sampleFishes
@@ -76,7 +83,11 @@
                 </div>
                 <Order fishes={this.state.fishes} order={this.state.order}
                     params={this.props.params}/>
-                <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+                <Inventory
+                    addFish={this.addFish}
+                    loadSamples={this.loadSamples}
+                    fishes={this.state.fishes}
+                    updateFish={this.updateFish}/>
             </div>
          )
      }
